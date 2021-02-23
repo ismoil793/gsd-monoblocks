f4e6ec97b312e0a28c59181b61c67b18858e17a3
ismoil793 <ismoil.793@gmail.com>
2021-02-23 13:27:34 +0500

    email verify page

--- pages/email/verify/[id].js
+++ pages/email/verify/[id].js
@@ -14,9 +14,7 @@ const VerifyEmail = () => {
              <div className="row mt-5">
                 <div className="col-12 mt-5 p-0">
                    <div style={{ height: '40vh' }}>
-                      <h1 className="heading-dark">
-                         Подтверждение аккаунта
-                      </h1>
+                      <h1 className="heading-dark">Подтверждение аккаунта</h1>
                    </div>
                 </div>
              </div>
