8b22f76f3e5d623ed01a048dd7dbc9fc2e616902
ismoil793 <ismoil.793@gmail.com>
2021-02-24 22:32:17 +0500

    changes hope will work

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withCSS = require('@zeit/next-css');
 const withFonts = require('next-fonts');
 module.exports = withFonts(withCSS({
   enableSvg: true,
-  distDir: 'build',
   webpack: function (config) {
     config.module.rules.push({
       test: /\.(eot|woff|woff2|otf|ttf|svg|png|jpg|gif)$/,
